91579b3a93d3c58c9a9538e01314d7d4e9b62bd8
slimane-lakehal <lakehalslimane@gmail.com>
2025-03-21 22:40:47 +0100

    Prediction pipeline and flask app

--- src/utils.py
+++ src/utils.py
@@ -40,12 +40,21 @@ def evaluate_model(X_train, y_train, X_test, y_test, models, params):
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
             
-            train_model_score = r2_score(y_train, y_train_pred)
+            # train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = r2_score(y_test, y_test_pred)
             
             report[list(models.keys())[i]] = test_model_score
         
         return report
     
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+
     except Exception as e:
         raise CustomException(e, sys)
